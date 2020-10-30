cf28bf047b3428344319b62cb676404f88deadea
lean <lea0010@hotmail.com>
2020-10-30 08:28:51 -0300

    creacion de Regmail

---
+++ app/Http/Livewire/Mail/Regmail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Mail;
+
+use Livewire\Component;
+
+class Regmail extends Component
+{
+    public function render()
+    {
+        return view('livewire.mail.regmail');
+    }
+}
