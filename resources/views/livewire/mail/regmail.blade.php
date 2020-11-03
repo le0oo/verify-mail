f09355414e8e2c0af90e11817b9329d76bfe140a
Lean <leo.rabadan@hotmail.com>
2020-11-03 20:10:17 -0300

    Form Register Mail

--- resources/views/livewire/mail/regmail.blade.php
+++ resources/views/livewire/mail/regmail.blade.php
@@ -5,7 +5,6 @@
                 <div class="shadow overflow-hidden sm:rounded-md">
                     <div class="px-4 py-5 bg-white sm:p-3">
                         <div class="grid grid-cols-6 gap-6">
-
                             <div class="col-span-12 sm:col-span-6">
                                 <label for="first_name" 
                                     class="block text-sm font-medium leading-5 text-gray-700">
@@ -14,7 +13,6 @@
                                 <input id="first_name"  wire:model="mail"
                                     class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
                             </div>
-            
                             <div class="col-span-10 sm:col-span-5">
                                 <label for="last_name" 
                                     class="block text-sm font-medium leading-5 text-gray-700">CIS #1
