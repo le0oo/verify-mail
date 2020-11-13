da90a34679015a92de253fda26944edb29ffb0da
lean <lea0010@hotmail.com>
2020-11-13 12:53:07 -0300

    se armo mail para mandar al cliente

---
+++ resources/views/mail/confirmacion-verifymail4.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <div align="center">
+      <img src="http://verify-mail.test/img/logo_grupoJunin.jpg"/>
+  
+      <font size=4 color="#17676E" face="Montserrat, Trebuchet, sans-serif">
+        <h3>Grupo Servicios Junín S. A.</h3>
+      </font>
+  
+      <font size=4 color="#17676E" face="Montserrat, Trebuchet, sans-serif">
+        <h3>Te agradece tu incorporación a la Factura Digital</h3>
+      </font>
+  
+      <a href="https://grupojunin.com.ar">
+        <img src="http://verify-mail.test/img/btn.jpg"/>
+      </a>
+  
+      </br>
+      <a href="https://grupojunin.com.ar">
+        <img src="http://verify-mail.test/img/pie.jpg"/>
+      </a>
+    </div>
+  </body>
+</html>
